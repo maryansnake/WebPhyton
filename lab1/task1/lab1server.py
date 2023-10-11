76c9d1f4bcc344e727f6ab90ecd4aa17828debce
Marian Starovoitov <maryansnake@gmail.com>
2023-10-11 22:31:25 +0300

    task2

--- lab1/task1/lab1server.py
+++ lab1/task1/lab1server.py
@@ -12,11 +12,20 @@ print(f"Сервер слухає на {host}:{port}...")
 
 client_socket, client_address = server_socket.accept()
 print(f"З'єднано з {client_address}")
-
 data = client_socket.recv(1024).decode('utf-8')
-current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-print(f"Отримано від клієнта ({client_address}): {data}")
-print(f"Час отримання: {current_time}")
+
+import time
+time.sleep(5)
+
+if data:
+    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(f"Отримано від клієнта ({client_address}): {data}")
+    print(f"Час отримання: {current_time}")
+    response = "Дані успішно отримано."
+else:
+    response = "Помилка при отриманні даних."
+
+client_socket.send(response.encode('utf-8'))
 
 client_socket.close()
 server_socket.close()
