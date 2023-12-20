f0f111ef919bf3b4dec267418f15a4eeabb91401
Marian Starovoitov <maryansnake@gmail.com>
2023-12-20 20:20:16 +0200

    Independ Work 2 done

--- other lab works/app/api/__init__.py
+++ other lab works/app/api/__init__.py
@@ -2,4 +2,7 @@ from flask import Blueprint
 
 api_bp = Blueprint('api', __name__)
 
+from .cars import api_cars_bp
+api_bp.register_blueprint(api_cars_bp, url_prefix="/cars")
+
 from . import controller
